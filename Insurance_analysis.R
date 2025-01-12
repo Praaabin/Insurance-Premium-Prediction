8e50aafa7d5ee756f19f3b8c9dc64cd5936c5b45
Praaabin <2308806@live.stmarys.ac.uk>
2025-01-12 00:37:43 +0000

    Initial commit: Start fresh

---
+++ Insurance_analysis.R
@@ -0,0 +1,10 @@
+# --------------------------
+# 1. Load Necessary Libraries
+# --------------------------
+
+library(tidyverse)  # For data manipulation and visualization
+library(caret)      # For data splitting
+library(randomForest)  # For Random Forest model
+library(gbm)        # For Gradient Boosting model
+library(ggplot2)    # For data visualization
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Insurance Premium Prediction  
+Predict health insurance premiums using customer data. Includes data preprocessing, EDA, and predictive models (Linear Regression, Random Forest, Gradient Boosting). Best model: Gradient Boosting (R² = 0.83). Key factors: smoking, BMI, age.
